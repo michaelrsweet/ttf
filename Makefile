6204e063d61782be27a321d4b8e41391dc24b9bb
Michael R Sweet <msweet@msweet.org>
2021-06-13 13:05:28 -0400

    Save work.

---
+++ Makefile
@@ -0,0 +1,78 @@
+#
+# Unit test makefile for TTF library.
+#
+#     https://github.com/michaelrsweet/ttf
+#
+# Copyright © 2021 by Michael R Sweet.
+#
+# Licensed under Apache License v2.0.  See the file "LICENSE" for more
+# information.
+#
+
+VERSION	=	1.0
+prefix	=	$(DESTDIR)/usr/local
+includedir =	$(prefix)/include
+libdir	=	$(prefix)/lib
+mandir	=	$(prefix)/share/man
+
+AR	=	ar
+ARFLAGS	=	rc
+CC	=	gcc
+CFLAGS	=	$(OPTIM) -Wall '-DVERSION="$(VERSION)"'
+LDFLAGS	=	$(OPTIM)
+LIBS	=
+LIBTTF	=	libttf.a
+OBJS	=	testttf.o ttf.o
+OPTIM	=	-Os -g
+RANLIB	=	ranlib
+TARGETS	=	testttf $(LIBTTF)
+
+.SUFFIXES:	.c .o
+.c.o:
+	$(CC) $(CFLAGS) -c -o $@ $<
+
+
+all:		$(TARGETS)
+
+
+clean:
+	rm -f $(TARGETS) $(OBJS)
+
+
+install:	all
+	mkdir -p $(includedir)
+	cp ttf.h $(includedir)
+	mkdir -p $(libdir)
+	cp $(LIBTTF) $(libdir)
+	if test $(LIBTTF) = libttf.a; then \
+		$(RANLIB) $(libdir)/libttf.a; \
+	fi
+	mkdir -p $(mandir)/man3
+	cp ttf.3 $(mandir)/man3
+
+
+test:		testttf
+	./testttf
+
+
+# Scan the code using Cppcheck <http://cppcheck.sourceforge.net>
+cppcheck:
+	cppcheck --template=gcc --addon=cert.py --suppress=cert-MSC24-C --suppress=cert-EXP05-C --suppress=cert-API01-C $(OBJS:.o=.c) 2>cppcheck.log
+	@test -s cppcheck.log && (echo ""; echo "Errors detected:"; echo ""; cat cppcheck.log; exit 1) || exit 0
+
+
+sanitizer:
+	$(MAKE) clean
+	$(MAKE) OPTIM="-g -fsanitize=address" all
+
+
+testttf:	ttf.o testttf.o
+	$(CC) $(LDFLAGS) -o testttf ttf.o testttf.o $(LIBS)
+
+
+libttf.a:	ttf.o
+	$(AR) $(ARFLAGS) $@ ttf.o
+	$(RANLIB) $@
+
+
+$(OBJS):	ttf.h Makefile
---
+++ testttf.c
@@ -0,0 +1,191 @@
+//
+// Unit test program for TTF library
+//
+//     https://github.com/michaelrsweet/ttf
+//
+// Copyright © 2018-2021 by Michael R Sweet.
+//
+// Licensed under Apache License v2.0.  See the file "LICENSE" for more
+// information.
+//
+// Usage:
+//
+//   ./testttf [FILENAME]
+//
+
+//
+// Include necessary headers...
+//
+
+#include <stdio.h>
+#include "ttf.h"
+
+
+//
+// Local functions...
+//
+
+static void	error_cb(void *data, const char *message);
+static int	test_font(const char *filename);
+
+
+//
+// 'main()' - Main entry for unit tests.
+//
+
+int					// O - Exit status
+main(int  argc,				// I - Number of command-line arguments
+     char *argv[])			// I - Command-line arguments
+{
+  int		i;			// Looping var
+  int		errors = 0;		// Number of errors
+
+
+  if (argc > 1)
+  {
+    for (i = 1; i < argc; i ++)
+      errors += test_font(argv[i]);
+  }
+  else
+  {
+    // Test with the bundled TrueType files...
+    errors += test_font("testfiles/OpenSans-Bold.ttf");
+    errors += test_font("testfiles/OpenSans-Regular.ttf");
+  }
+
+  return (errors);
+}
+
+
+//
+// 'error_cb()' - Error callback.
+//
+
+static void
+error_cb(void       *data,		// I - User data (not used)
+         const char *message)		// I - Message string
+{
+  fprintf(stderr, "FAIL (%s)\n", message);
+}
+
+
+//
+// 'test_font()' - Test a font file.
+//
+
+static int				// O - Number of errors
+test_font(const char *filename)		// I - Font filename
+{
+  int		errors = 0;		// Number of errors
+  ttf_t		*font;			// Font
+  const char	*value;			// Font value
+  ttf_rect_t	extents;		// Extents
+  size_t	num_fonts;		// Number of fonts
+  ttf_style_t	style;			// Font style
+  ttf_weight_t	weight;			// Font weight
+  static const char * const styles[] =	// Font style names
+  {
+    "TTF_STYLE_NORMAL",
+    "TTF_STYLE_ITALIC",
+    "TTF_STYLE_OBLIQUE"
+  };
+
+
+  printf("ttfCreate(\"%s\"): ", filename);
+  if ((font = ttfCreate(filename, 0, error_cb, NULL)) != NULL)
+    puts("PASS");
+  else
+    errors ++;
+
+  fputs("ttfGetCopyright: ", stdout);
+  if ((value = ttfGetCopyright(font)) != NULL)
+  {
+    printf("PASS (%s)\n", value);
+  }
+  else
+  {
+    puts("FAIL");
+    errors ++;
+  }
+
+  fputs("ttfGetExtents: ", stdout);
+  if (ttfGetExtents(font, 12.0f, "Hello, World!", &extents))
+  {
+    printf("PASS (%g %g %g %g)\n", extents.left, extents.top, extents.right, extents.bottom);
+  }
+  else
+  {
+    puts("FAIL");
+    errors ++;
+  }
+
+  fputs("ttfGetFamily: ", stdout);
+  if ((value = ttfGetFamily(font)) != NULL)
+  {
+    printf("PASS (%s)\n", value);
+  }
+  else
+  {
+    puts("FAIL");
+    errors ++;
+  }
+
+  fputs("ttfGetNumFonts: ", stdout);
+  if ((num_fonts = ttfGetNumFonts(font)) > 0)
+  {
+    printf("PASS (%u)\n", (unsigned)num_fonts);
+  }
+  else
+  {
+    puts("FAIL");
+    errors ++;
+  }
+
+  fputs("ttfGetPostScriptName: ", stdout);
+  if ((value = ttfGetPostScriptName(font)) != NULL)
+  {
+    printf("PASS (%s)\n", value);
+  }
+  else
+  {
+    puts("FAIL");
+    errors ++;
+  }
+
+  fputs("ttfGetStyle: ", stdout);
+  if ((style = ttfGetStyle(font)) >= TTF_STYLE_NORMAL && style <= TTF_STYLE_ITALIC)
+  {
+    printf("PASS (%s)\n", styles[style]);
+  }
+  else
+  {
+    puts("FAIL");
+    errors ++;
+  }
+
+  fputs("ttfGetVersion: ", stdout);
+  if ((value = ttfGetVersion(font)) != NULL)
+  {
+    printf("PASS (%s)\n", value);
+  }
+  else
+  {
+    puts("FAIL");
+    errors ++;
+  }
+
+  fputs("ttfGetWeight: ", stdout);
+  if ((weight = ttfGetWeight(font)) >= 0)
+  {
+    printf("PASS (%u)\n", (unsigned)weight);
+  }
+  else
+  {
+    puts("FAIL");
+    errors ++;
+  }
+
+  ttfDelete(font);
+
+  return (errors);
+}
